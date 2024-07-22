9d6bc79de733babc08340be706cce1c18515d165
varshasri-g <148864505+varshasri-g@users.noreply.github.com>
2024-07-22 12:11:33 +0530

    Update app10.py

--- app10.py
+++ app10.py
@@ -7,7 +7,7 @@ from docx import Document
 
 # Load the Excel file
 def a10():
-    xls = pd.ExcelFile(r"C:\Users\Guest_User\Downloads\Data.xlsx")
+    xls = pd.ExcelFile(r"Data.xlsx")
 
     # Load the sheet with the questions
     sheet_name ='10.Transportation Management'  # Adjust if necessary
