da0a3c5ebb0e1326cd6ee4e542b71a8c82b04554
varshasri-g <148864505+varshasri-g@users.noreply.github.com>
2024-07-22 12:13:47 +0530

    Update app12.py

--- app12.py
+++ app12.py
@@ -7,7 +7,7 @@ from docx import Document
 
 # Load the Excel file
 def a12():
-    xls = pd.ExcelFile(r"C:\Users\Guest_User\Downloads\Data.xlsx")
+    xls = pd.ExcelFile("Data.xlsx")
 
     # Load the sheet with the questions
     sheet_name ='12.Finance'  # Adjust if necessary
